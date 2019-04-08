557a10acf3f35f188cfb3e8df7370566a2e89d2b
joris <j.j.dejosselindejong@gmail.com>
2019-04-08 20:22:53 +0200

    remove unnesecarry files

--- docker/files/compose/wordpress/letsencrypt.sh
+++
@@ -1,8 +0,0 @@
-apt update
-apt install php git wget certbot php-soap -y
-
-certbot --manual-public-ip-logging-ok --non-interactive --agree-tos -m j.j.dejosselindejong@gmail.com --server https://acme-v02.api.letsencrypt.org/directory \
-certonly --manual --preferred-challenges=dns \
---manual-auth-hook /certbot-transip-dns-01-validator/auth-hook \
---manual-cleanup-hook /certbot-transip-dns-01-validator/cleanup-hook \
--d 'joict.nl' -d '*.joict.nl'
